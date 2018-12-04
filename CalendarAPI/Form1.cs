045a8bb6942eb3b624cdc1bb19e1d8681576351f
Burak KARADAŞ <burak@atilimyazilim.com>
2018-12-04 14:19:29 +0300

    Initial commit

---
+++ CalendarAPI/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+namespace CalendarAPI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UserCredential credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                new ClientSecrets
+                {
+                    ClientId = "125967172436-fd5h9bap8bju9s010cjo32345gdcq7n4.apps.googleusercontent.com",
+                    ClientSecret = "pZ-Y1YggrSGPXakKVcOmwrPf",
+                },
+                new[] { CalendarService.Scope.Calendar },
+                "user",
+                CancellationToken.None).Result;
+
+            // Create the service.
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Calendar API Sample",
+            });
+            Event newEvent = new Event()
+            {
+                Summary = "Google I/O 2015",
+                Location = "800 Howard St., San Francisco, CA 94103",
+                Description = "A chance to hear more about Google's developer products.",
+                Start = new EventDateTime()
+                {
+                    DateTime = DateTime.Now,
+                    TimeZone = "Europe/Istanbul",
+                },
+                End = new EventDateTime()
+                {
+                    DateTime = DateTime.Now.AddHours(1),
+                    TimeZone = "Europe/Istanbul",
+                },
+                Attendees = new EventAttendee[] {
+        new EventAttendee() { Email = "ali@atilimyazilim.com" },
+    },
+                Reminders = new Event.RemindersData()
+                {
+                    UseDefault = false,
+                    Overrides = new EventReminder[] {
+            new EventReminder() { Method = "email", Minutes = 24 * 60 },
+            //new EventReminder() { Method = "sms", Minutes = 10 },
+        }
+                }
+            };
+
+            String calendarId = "primary";
+            EventsResource.InsertRequest request = service.Events.Insert(newEvent, calendarId);
+            Event createdEvent = request.Execute();
+        }
+    }
+}
